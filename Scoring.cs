20d49d51fcfd55b72c57cadc1cfac45a8302981b
Abdirahman Omar Hashi <ohashi@gtu.edu.tr>
2018-01-16 11:45:58 -0800

    Create Scoring.cs

---
+++ Scoring.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoring : MonoBehaviour {
+
+    // Use this for initialization
+    public static int score;
+}
